ba9e99d7ec853008dd1953352cc3503f0e4bfa4a
PablitoCortes <cjuan1597@gmail.com>
2024-12-05 21:27:38 -0500

    asdf

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,12 @@
 import { Product } from "@/interfaces/Product";
 import ProductCard from "@/components/ProductCard";
 import { getProducts } from "@/services/productService";
-import styles from "./home.module.css";
+
 const Home: React.FC<{ products: Product[] }> = async () => {
   const products = await getProducts();
   const hotProducts = products.slice(0, 3);
   return (
-    <div className={styles.container}>
+    <div>
       <h1>Hot Sale</h1>
       <div>
         {hotProducts.map((product) => (
